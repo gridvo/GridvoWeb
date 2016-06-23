c8581b7821c285a4dce4ce84d7bee699e981ffea
linmadan <772181827@qq.com>
2016-06-23 14:35:35 +0800

    集成微信企业号功能

--- routes/weChat.js
+++ routes/weChat.js
@@ -1,26 +1,24 @@
 'use strict';
 var _ = require('underscore');
 var express = require('express');
-var bodyParser = require('body-parser');
 var WeChatCrypto = require('wechat-crypto');
 
 var router = express.Router();
-router.use(bodyParser.urlencoded({extended: false}));
 router.get('/sysevent', function (req, res) {
     var corpID = "wx9b41eed392c6d447";
     var token = "GuEccJzML5RxVB8fLhhzRPBda5aJNw5J";
     var encodingAESKey = "FWj4LPoWHVbHglyHnxC4gifhjFfOn89hfYXMH3Y7N9b";
     var weChatCrypto = new WeChatCrypto(token, encodingAESKey, corpID);
-    var signature = req.body.msg_signature;
-    var timestamp = req.body.timestamp;
-    var nonce = req.body.nonce;
-    var isWeChatServerAuthURL = !_.isUndefined(req.body.echostr);
+    var signature = req.query.msg_signature;
+    var timestamp = req.query.timestamp;
+    var nonce = req.query.nonce;
+    var isWeChatServerAuthURL = !_.isUndefined(req.query.echostr);
     if (isWeChatServerAuthURL) {
-        let encrypt = req.body.echostr;
+        let encrypt = req.query.echostr;
         let auth_signature = weChatCrypto.getSignature(timestamp, nonce, encrypt);
         if (signature == auth_signature) {
             let echostr = weChatCrypto.decrypt(encrypt);
-            console.log(echostr);
+            res.send(echostr.message);
         }
     } else {
         console.log("sys event");
