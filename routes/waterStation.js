b974ca64fa916be51fbba431951fd83cd486e0cb
linmadan <772181827@qq.com>
2016-06-23 15:44:21 +0800

    集成微信企业号功能

--- routes/waterStation.js
+++ routes/waterStation.js
@@ -5,7 +5,24 @@ var WeChatCrypto = require('wechat-crypto');
 
 var router = express.Router();
 router.get('/user-event', function (req, res) {
-    console.log(req.query);
+    var corpID = "wx9b41eed392c6d447";
+    var token = "GuEccJzML5RxVB8fLhhzRPBda5aJNw5J";
+    var encodingAESKey = "FWj4LPoWHVbHglyHnxC4gifhjFfOn89hfYXMH3Y7N9b";
+    var weChatCrypto = new WeChatCrypto(token, encodingAESKey, corpID);
+    var signature = req.query.msg_signature;
+    var timestamp = req.query.timestamp;
+    var nonce = req.query.nonce;
+    var isWeChatServerAuthURL = !_.isUndefined(req.query.echostr);
+    if (isWeChatServerAuthURL) {
+        let encrypt = req.query.echostr;
+        let auth_signature = weChatCrypto.getSignature(timestamp, nonce, encrypt);
+        if (signature == auth_signature) {
+            let echostr = weChatCrypto.decrypt(encrypt);
+            res.send(echostr.message);
+        }
+    } else {
+        console.log("user event");
+    }
 });
 
 module.exports = router;
